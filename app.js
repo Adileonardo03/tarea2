f177dede232e1e94ebeb85116033b4af21e9021a
Adileonardo03 <adileonardo03@gmail.com>
2024-08-26 21:50:01 -0600

    subimos el programa de tarea 2

---
+++ app.js
@@ -0,0 +1,4 @@
+Let numero1 = prompt( "ingresa el numero 1");
+Let numero2 = prompt ("ingresa el numero 2");
+Let resultado = parseInt (numero1) + parseInt (numero2);
+Console.log ("tu resultado es:" + resultado);
